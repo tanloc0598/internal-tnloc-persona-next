45132c8e2b5ee085dda193b60d3abceba7d0a1bf
Nguyen Tan Loc <tnloc@outlook.com>
2022-12-01 13:50:51 +0700

    [add] test icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,16 @@ import MainLayout from '../components/layouts/mainLayout'
 import type { NextPageWithLayout } from './_app'
 
 import type { ReactElement } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 const Index: NextPageWithLayout = () => {
-  return <p>hello world</p>
+  return <div><p>
+    hello world
+    
+    </p>
+    <FontAwesomeIcon icon={faEnvelope} />
+ </div>
 }
 
 Index.getLayout = function getLayout(page: ReactElement) {
